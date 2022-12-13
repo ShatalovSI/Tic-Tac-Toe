020c616d589773b5d1ac4493460b749d1aa46ac4
ShatalovSI <shatalov_s_i@mail.ru>
2022-12-14 01:49:37 +0300

    add function main

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,10 @@
 import tictactoe
 
 
-game = tictactoe.TicTacToeGame('Игрок1', 'Игрок2')
-game.start_game()
+def main():
+    game = tictactoe.TicTacToeGame('Игрок1', 'Игрок2')
+    game.start_game()
+
+
+if __name__ == '__main__':
+    main()
